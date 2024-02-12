f58989534fdca453d674bbc5ba55404c7a385760
Idle <651462+reonZ@users.noreply.github.com>
2024-02-12 13:09:04 +0100

    remove already accessible classes

--- src/api.ts
+++ src/api.ts
@@ -1,19 +1,4 @@
-import { ChatMessagePF2e } from "@module/chat-message/document.ts";
-import { DamageRoll } from "@system/damage/roll.ts";
-
 Object.defineProperties(window, {
-    ChatMessagePF2e: {
-        value: ChatMessagePF2e,
-        writable: false,
-        configurable: false,
-        enumerable: false,
-    },
-    DamageRoll: {
-        value: DamageRoll,
-        writable: false,
-        configurable: false,
-        enumerable: false,
-    },
     pf2eApi: {
         value: {
             rules: await import("@module/rules/helpers.ts"),
